a527000471ac12f0c9dd926d7b288e158b10dad3
Anderson Mills <anderson@puppet.com>
2017-05-23 11:57:47 -0500

    Read command line arguments.

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-modmod
+# modmod
 
-be mod. 
+be mod.
+
+## Intended usage:
+`modmod username-modname newversion`
+
+will update all `username-modname` modules to use ref or version `newversion`
--- src/modmod.rs
+++ src/modmod.rs
@@ -1,3 +1,28 @@
+extern crate rustc_serialize;
+extern crate docopt;
+
+use docopt::Docopt;
+
+const USAGE: &'static str = "
+modmod
+
+Usage:
+  modmod <username-modname> <newversion>
+  modmod ( -h | --help )
+
+Options:
+  -h --help     Show this screen.
+";
+
+#[derive(Debug, RustcDecodable)]
+struct Args {
+    arg_username_modname: String,
+    arg_newversion: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Args = Docopt::new(USAGE)
+                            .and_then(|d| d.decode())
+                            .unwrap_or_else(|e| e.exit());
+    println!("{:?}", args);
 }

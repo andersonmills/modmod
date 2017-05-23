8f2f0c7dc40708b458d471574a7051fde7a6dc8a
Anderson Mills <anderson@puppet.com>
2017-05-23 14:16:15 -0500

    Add git dependency and show branches in .

--- src/modmod.rs
+++ src/modmod.rs
@@ -1,13 +1,19 @@
 extern crate rustc_serialize;
 extern crate docopt;
+extern crate git2;
 
+use git2::Repository;
 use docopt::Docopt;
 
 const USAGE: &'static str = "
 modmod
 
+  updates a single module across all Puppetfiles in a control-repo.
+  It is intended to be run in the root dir of a control-repo.
+
+
 Usage:
-  modmod <username-modname> <newversion>
+  modmod <username-modname> <version>
   modmod ( -h | --help )
 
 Options:
@@ -17,7 +23,7 @@ Options:
 #[derive(Debug, RustcDecodable)]
 struct Args {
     arg_username_modname: String,
-    arg_newversion: String,
+    arg_version: String,
 }
 
 fn main() {
@@ -25,4 +31,41 @@ fn main() {
                             .and_then(|d| d.decode())
                             .unwrap_or_else(|e| e.exit());
     println!("{:?}", args);
+
+    let repo = match Repository::open(".") {
+            Ok(repo) => repo,
+            Err(e) => panic!("failed to open: {}", e),
+    };
+
+    let result = repo.branches(Some(git2::BranchType::Local));
+
+    let branches = match result {
+            Ok(branches) => branches,
+            Err(e) => panic!("failed to find branches: {}", e),
+    };
+
+    for (i, result2) in branches.enumerate() {
+        println!("number {}", i);
+        let branch_tuple = match result2 {
+            Ok(branch) => branch,
+            Err(e) => panic!("failed to find branch: {}", e),
+        };
+
+        let branch_option = match branch_tuple.0.name() {
+            Ok(option) => option,
+            Err(e) => panic!("failed to find branch name: {}", e),
+        };
+
+        let branch_name = match branch_option {
+            Some(x) => x,
+            None    => "No branch name was found.",
+        };
+
+        println!("branch {}", branch_name);
+        //match option {
+            //Some(x) => println!("Result: {}", x.name()),
+            //None    => println!("Cannot divide by 0"),
+        //};
+        //println!("{:?}", branch.ok());
+    }
 }
